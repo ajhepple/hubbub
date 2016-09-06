5488b0292184731d4b466b617e7e4981754192c9
Anthony Hepple <anthony@dhdevelopment.co.uk>
2016-09-06 13:23:18 +0100

    Added register action to User controller.
    	modified:   grails-app/controllers/com/grailsinaction/UserController.groovy
    	new file:   grails-app/views/user/register.gsp

--- grails-app/controllers/com/grailsinaction/UserController.groovy
+++ grails-app/controllers/com/grailsinaction/UserController.groovy
@@ -26,4 +26,39 @@ class UserController {
         }
         return [profiles: profiles]
     }
+
+    def register () {
+        if (request.method == "POST") {
+            def user = new User(params)
+            if (user.validate()) {
+                user.save()
+                flash.message = "Successfully Created User"
+                redirect(uri: '/')
+            } else {
+                flash.message = "Error Registering User"
+                return [user: user]
+            }
+        }
+    }
+
+    def update () {
+        // Get the logged-in user and attach it to this request's scope
+        // TODO ** Not implemented yet **
+        def user = session.user?.attach()
+
+        if (user) {
+            // update the user with a blacklist technique
+            bindDate(user, params, ['loginId','password'])
+            // update their profile using a whitelist technique
+            user.profile.properties['email','fullName'] = params
+            if (user.save()) {
+                flash.message = "Successfully updated user"
+            } else {
+                flash.message = "Failed to update user"
+            }
+            [user: user]
+        } else {
+            response.sendError(404)
+        }
+    }
 }
